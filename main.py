655f0cffdb8982955f451309c7ef7943377f5755
SONG <simpledeveloper.songs@gmail.com>
2022-01-04 18:15:19 +0900

    download file page

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import uvicorn
 
 from typing import List
 
-from fastapi import FastAPI, File, UploadFile, HTTPException
-from fastapi.responses import HTMLResponse, Response
+from fastapi import FastAPI, File, UploadFile, HTTPException, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 file_lst = []
 
@@ -22,6 +24,11 @@ async def create_upload_files(files: List[UploadFile] = File(...)):
         return HTTPException(status_code=404, detail="아직 여러개의 파일은 올릴 수 없습니다.")
 
 
+@app.get("/downloadfile", response_class=HTMLResponse)
+async def download_file(request: Request):
+    return templates.TemplateResponse("item.html", {"request": request})
+
+
 @app.get("/")
 async def main():
     content = """
@@ -29,6 +36,13 @@ async def main():
 <form action="/uploadfiles/" enctype="multipart/form-data" method="post">
 <input name="files" type="file" multiple>
 <input type="submit">
+
+<br>
+<br>
+<br>
+
+<form action="/downloadfile/" method="get">
+<button type="submit">다운로드</button>
 </form>
 </body>
     """
